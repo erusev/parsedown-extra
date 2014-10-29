e81515ed5549871b16ece3aaab8cf023930b1e7e
Emanuil Rusev <hello@erusev.com>
2014-10-29 03:21:00 +0200

    make sure tests run on Travis CI

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
-include '../parsedown/Parsedown.php';
-include '../parsedown/test/ParsedownTest.php';
+$dir = file_exists('../parsedown/')
+    ? '../parsedown/' # child
+    : 'vendor/erusev/parsedown/'; # parent
+
+include $dir . 'Parsedown.php';
+include $dir . 'test/ParsedownTest.php';
+
 include 'ParsedownExtra.php';
