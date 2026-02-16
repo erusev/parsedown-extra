5b5b9b9d000792c7ac631aec23a6688e0b6d3dc5
Emanuil Rusev <hello@erusev.com>
2026-02-16 15:06:37 +0200

    Minor cleanups: use https for homepage and add trailing newline

--- ParsedownExtra.php
+++ ParsedownExtra.php
@@ -6,7 +6,7 @@
 # https://github.com/erusev/parsedown-extra
 #
 # (c) Emanuil Rusev
-# http://erusev.com
+# https://erusev.com
 #
 # For the full license information, view the LICENSE file that was distributed
 # with this source code.
