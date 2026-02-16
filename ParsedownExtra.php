970b8b37c661cde2b4e3372c6a590a75ed3eff3d
Emanuil Rusev <hello@erusev.com>
2026-02-16 14:06:59 +0200

    Fix PHP 8.2+ deprecations in ParsedownExtra
    Replace mb_convert_encoding(..., 'HTML-ENTITIES', 'UTF-8') with mb_encode_numericentity() before DOMDocument::loadHTML.
    
    Reasoning: PHP 8.2 deprecates mbstring HTML-ENTITIES handling; mb_encode_numericentity is the recommended replacement and preserves markup while converting non-ASCII code points, which keeps DOM parsing behavior intact without warnings on PHP 8.4/8.5.
    
    Also replace relative callable 'self::sortFootnotes' with [, 'sortFootnotes'] to avoid PHP 8.2 relative-callable deprecation.

--- ParsedownExtra.php
+++ ParsedownExtra.php
@@ -508,7 +508,7 @@ class ParsedownExtra extends Parsedown
             ),
         );
 
-        uasort($this->DefinitionData['Footnote'], 'self::sortFootnotes');
+        uasort($this->DefinitionData['Footnote'], [$this, 'sortFootnotes']);
 
         foreach ($this->DefinitionData['Footnote'] as $definitionId => $DefinitionData)
         {
@@ -625,7 +625,11 @@ class ParsedownExtra extends Parsedown
         $DOMDocument = new DOMDocument;
 
         # http://stackoverflow.com/q/11309194/200145
-        $elementMarkup = mb_convert_encoding($elementMarkup, 'HTML-ENTITIES', 'UTF-8');
+        $elementMarkup = mb_encode_numericentity(
+            $elementMarkup,
+            [0x80, 0x10FFFF, 0, ~0],
+            'UTF-8'
+        );
 
         # http://stackoverflow.com/q/4879946/200145
         $DOMDocument->loadHTML($elementMarkup);
